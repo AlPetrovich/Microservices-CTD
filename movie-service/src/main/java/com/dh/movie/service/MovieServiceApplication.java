629a53efb0bf902670880ff94b2aaba366a0278e
alpetrovich <alexispetrovich11@gmail.com>
2022-09-19 10:48:43 -0300

    add properties rabbitmq

--- movie-service/src/main/java/com/dh/movie/service/MovieServiceApplication.java
+++ movie-service/src/main/java/com/dh/movie/service/MovieServiceApplication.java
@@ -1,10 +1,12 @@
 package com.dh.movie.service;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableRabbit
 @SpringBootApplication
 public class MovieServiceApplication {
 
